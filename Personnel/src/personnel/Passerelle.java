ba2695127941c3b59a8374307e1e9dd96c5dee0c
valentinsmck <144223165+valentinsmck@users.noreply.github.com>
2025-11-17 14:16:24 +0100

    Delete Personnel directory

--- Personnel/src/personnel/Passerelle.java
+++
@@ -1,8 +0,0 @@
-package personnel;
-
-public interface Passerelle 
-{
-	public GestionPersonnel getGestionPersonnel();
-	public void sauvegarderGestionPersonnel(GestionPersonnel gestionPersonnel)  throws SauvegardeImpossible;
-	public int insert(Ligue ligue) throws SauvegardeImpossible;
-}
