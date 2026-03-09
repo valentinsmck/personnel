d302b18765d0663b0f57e11004ce1140087b5f30
valentinsmck <144223165+valentinsmck@users.noreply.github.com>
2026-03-09 16:20:47 +0100

    feat(db): change type passerelle (serialization) into JDBC

--- Personnel/src/personnel/GestionPersonnel.java
+++ Personnel/src/personnel/GestionPersonnel.java
@@ -23,7 +23,7 @@ public class GestionPersonnel implements Serializable
 	private SortedSet<Ligue> ligues;
 	private Employe root;
 	public final static int SERIALIZATION = 1, JDBC = 2, 
-			TYPE_PASSERELLE = SERIALIZATION;  
+			TYPE_PASSERELLE = JDBC;
 	private static Passerelle passerelle = TYPE_PASSERELLE == JDBC ? new jdbc.JDBC() : new serialisation.Serialization();	
 	
 	/**
